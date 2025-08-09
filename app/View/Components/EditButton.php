2d21ed7d72ff41c212e7887ce99828495b514202
DelisPassos <delis.passos.albuquerque05@aluno.ifce.edu.br>
2025-08-09 00:53:11 -0300

    atualizar

--- app/View/Components/EditButton.php
+++ app/View/Components/EditButton.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\View\Component;
 use Illuminate\Contracts\View\View;
 
-class edit-button extends Component
+class EditButton extends Component
 {
     /**
      * Create a new component instance.
